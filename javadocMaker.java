403a62b8f83a7762bdb709ce7fb9a2686ac2045e
Hudson Chromy <hjc0018@auburn.edu>
2018-11-05 13:26:56 -0600

    Bug fixes
    Creates constructor comments
    Doesn't make void return type comments

--- javadocMaker.java
+++ javadocMaker.java
@@ -5,11 +5,7 @@ import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;  
 import java.util.Date;
-/**
-*
-*@author: Hudson Chromy
-*@date: 10/19/2018
-*/
+
 
 public class javadocMaker {
    public static void main(String[] args) throws IOException {
@@ -26,6 +22,8 @@ public class javadocMaker {
       System.out.print("File Name: ");
       Scanner userInput = new Scanner(System.in);
       String fileName = userInput.nextLine();
+      String constructor = fileName.substring(0, fileName.indexOf("."));
+      //System.out.println(constructor);
       System.out.print("Would you like to label the comments? ");
       String decision = userInput.nextLine();
       if(decision.charAt(0) == 'y') {
@@ -33,69 +31,94 @@ public class javadocMaker {
       } else {
          LABEL = false;
       }
-      //establish date
+      
       SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
       Date date = new Date();
-      //establish file writer and scanner
       FileWriter fw = new FileWriter("new" + fileName);
       PrintWriter pw = new PrintWriter(fw);
       Scanner scanFile = new Scanner(new File(fileName));
       
-      //run through file
       while(scanFile.hasNextLine()) {
- 
+         //System.out.println("hello");
          nxt = scanFile.nextLine();
-         //check to do header
+         //System.out.println(nxt);
          if(nxt.length() > 6 && !nxt.substring(0, 6).equals("import") && date_done == false) {
             pw.println("/**\n*\n*author: " + NAME + "\n*date: " + formatter.format(date) + "\n*/");
             date_done = true;
          }
-         //check if this line is a class
-         if(nxt.trim().length() > 7 && nxt.trim().substring(0, 6).equals("public") && !nxt.trim().substring(7, 12).equals("class")) {
-            ends = nxt.lastIndexOf(" ", nxt.indexOf("("));
-            starts = nxt.lastIndexOf(" ", ends-1);
-            returnType = nxt.substring(starts, ends);
-            pw.println("\t/**");
-            //get label for return
-            if(LABEL) {
-               System.out.print(returnType + ": ");
-               label = userInput.nextLine();
+         if(nxt.trim().length() > 7 && nxt.trim().substring(0, 6).equals("public") && !nxt.trim().substring(7, 12).equals("class") && nxt.indexOf("{") != -1) {
+            if(nxt.indexOf(constructor) != -1) {
+               pw.println(constructorMaker(constructor, nxt, LABEL));
             }
-            //put in the return commment
-            pw.println("\t*@return" + returnType + " " + label);
-            //check to see if there are 
-            if(nxt.indexOf("()") == -1) {
-               ends = 0;
-               starts = nxt.indexOf("(") + 3;
-               starts = nxt.indexOf(" ", starts);
-               if(nxt.indexOf(",") != -1){
-                  while(nxt.indexOf(",", ends+1) != -1) {
-                     ends = nxt.indexOf(",", starts);
-                     //gets label for param
-                     if(LABEL) {
-                        System.out.print(nxt.substring(starts, ends) + ": ");
-                        label = userInput.nextLine();
-                     }
-                     pw.println("\t*@param" + nxt.substring(starts, ends) + " " + label);
-                     starts = nxt.indexOf(",", ends) + 3;
-                     starts = nxt.indexOf(" ", starts);
-                  }
-               }
-               ends = nxt.indexOf(")");
-               //get label for param
-               if(LABEL) {
-                  System.out.print(nxt.substring(starts, ends) + ": ");
-                  label = userInput.nextLine();
-               }
-               pw.println("\t*@param" + nxt.substring(starts, ends) + " " +label);
-               
-            }
-            //pw.println("\t*");
-            pw.println("\t*/");
+            else {
+            pw.println(methodMaker(nxt, LABEL, false));
+			   }
          }
          pw.println(nxt);
       }
-      pw.println("//Edited");
+      //pw.println("//Edited");
       pw.close();
    }
+   
+   static String constructorMaker (String constructorIn, String nxt, boolean LABEL) {
+      //System.out.println("-------");
+      return "/**\n*Constuctor for " + constructorIn + ".\n" + methodMaker(nxt, LABEL, true);
+   }
+   
+   static String methodMaker (String nxt, boolean LABEL, boolean construct) {
+      String returnType = "";
+      String label = "";
+      Scanner userInput = new Scanner(System.in);
+      String outp = "";
+      int ends = nxt.lastIndexOf(" ", nxt.indexOf("("));
+	   int starts =	nxt.lastIndexOf(" ",	ends-1);
+		System.out.println(nxt);
+		
+      if(!construct) {
+         returnType = nxt.substring(starts, ends);
+         outp += "\t/**\n";
+      }
+		
+		//get	label	for return
+		//System.out.println(returnType);
+      if(returnType != "" && !returnType.equals(" void")) {
+         if(LABEL) {
+		      System.out.print(returnType +	": ");
+			   label	= userInput.nextLine();
+		   }
+         
+		   outp += "\t*@return"	+ returnType +	" " +	label + "\n";
+      }
+		if(nxt.indexOf("()")	==	-1) {
+			ends = 0;
+			starts =	nxt.indexOf("(") + 3;
+			starts =	nxt.indexOf(" ", starts);
+		   if(nxt.indexOf(",") != -1){
+				while(nxt.indexOf(",", ends+1) != -1) {
+					ends = nxt.indexOf(",",	starts);
+					//gets label for param
+					if(LABEL) {
+						System.out.print(nxt.substring(starts,	ends)	+ ": ");
+						label	= userInput.nextLine();
+					}
+					outp += "\t*@param" + nxt.substring(starts, ends) + " " + label + "\n";
+					starts =	nxt.indexOf(",", ends) + 3;
+					starts =	nxt.indexOf(" ", starts);
+				}
+			}
+			ends = nxt.indexOf(")");
+			//get	label	for param
+			if(LABEL) {
+				System.out.print(nxt.substring(starts,	ends)	+ ": ");
+				label	= userInput.nextLine();
+			}
+			outp += "\t*@param" + nxt.substring(starts, ends) + " " +label + "\n";
+					
+		}
+		//pw.println("\t*");
+		outp += "\t*/\t";
+      return outp;
+	   
+   }
 }
+
